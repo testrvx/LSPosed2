5efc0b2007fe26aecbda8ae1722398b586cd93e5
DanGLES3 <switchtegra@gmail.com>
2024-07-28 14:22:41 -0300

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -57,22 +57,23 @@ cmaker {
 }
 
 val repo = jgit.repo()
-val commitCount = (repo?.commitCount("HEAD") ?: 1) + 4200
+val commitCount = (repo?.commitCount("refs/remotes/origin/main") ?: 1) + 4200
 val latestTag = repo?.latestTag?.removePrefix("v") ?: "1.0"
 
 val injectedPackageName by extra("com.android.shell")
 val injectedPackageUid by extra(2000)
 
 val defaultManagerPackageName by extra("org.lsposed.manager")
+val apiCode by extra(93)
 val verCode by extra(commitCount)
 val verName by extra(latestTag)
 val androidTargetSdkVersion by extra(34)
 val androidMinSdkVersion by extra(27)
 val androidBuildToolsVersion by extra("34.0.0")
 val androidCompileSdkVersion by extra(34)
-val androidCompileNdkVersion by extra("26.1.10909125")
-val androidSourceCompatibility by extra(JavaVersion.VERSION_17)
-val androidTargetCompatibility by extra(JavaVersion.VERSION_17)
+val androidCompileNdkVersion by extra("26.2.11394342")
+val androidSourceCompatibility by extra(JavaVersion.VERSION_21)
+val androidTargetCompatibility by extra(JavaVersion.VERSION_21)
 
 tasks.register("Delete", Delete::class) {
     delete(rootProject.layout.buildDirectory)
